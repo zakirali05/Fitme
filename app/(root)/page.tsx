3e6c9200e243d9dadba79b037b0c8fa07915ffa0
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2023-08-06 12:18:43 +0530

    second commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Contact } from '@/components/Contact'
 import { Gallery } from '@/components/Gallery'
 import { Hero } from '@/components/Hero'
@@ -7,18 +9,25 @@ import { TableComponent } from '@/components/TableComponent'
 import { Testimonials } from '@/components/Testimonials'
 import { Training } from '@/components/Training'
 import {Footer} from "@/components/Footer"
+import { useEffect, useState } from 'react'
 export default function Home() {
+  const [isMounted , setIsMounted] = useState(false)
+  useEffect(()=>{
+    setIsMounted(true)
+  },[])
   return (
-    <div>
-      <Navbar />
-      <SheetComponent />
-      <Hero />
-     <Training/>
-     <Gallery/>
-     <TableComponent/>
-     <Testimonials/>
-     <Contact/>
-     <Footer/>
-    </div>
+isMounted ? <> <div>
+  <Navbar />
+  <SheetComponent />
+  <Hero />
+ <Training/>
+ <Gallery/>
+ <TableComponent/>
+ <Testimonials/>
+ <Contact/>
+ <Footer/>
+</div></> : <></>
+
+   
   )
 }
