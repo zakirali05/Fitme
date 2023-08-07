e4d1d9627ffad053f366bfe5ae2de716f42e9e3d
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2023-08-07 11:48:56 +0530

    sixth commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 
   return (
 
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider   publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
 
         <body className={inter.className}>
